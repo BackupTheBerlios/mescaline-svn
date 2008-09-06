d3aa976008c5ff515928bf069d3c4ab01f9c017a
mkulke <mkulke@14bf44e4-104e-0410-9d61-ae8792c909cf>
2008-09-06 19:07:25 +0000

    - improved js code

--- trunk/js/layout.js
+++ trunk/js/layout.js
@@ -35,35 +35,49 @@ function makeMoveable(widgets, argumentString){
  		var widget = new dojo.dnd.Moveable(widgets[i], {handle: widgets[i] + "_handle"});
 
  		dojo.connect(widget, "onMoveStop", function(mover){
- 
- 			var arguments = updateArguments(widgets, argumentString);
- 			window.location.replace(arguments);
+
+			var pos = dojo.coords(mover.node);
+			//console.log(mover.node.id);
+			//console.log(pos.left);
+			//console.log(mover.node.style.left.replace("px", ""));
+
+			var x = mover.node.style.left.replace("px", "");
+			var y = mover.node.style.top.replace("px", "");
+			var width = mover.node.offsetWidth;
+
+			// 	Ensure that the handle does not move out of the browser window.
+
+			if (y < 0) y = 0;
+			if (x < (0 - width + 20)) x = 20 - width;
+
+			window.location.replace(argumentString + "&pos:" + mover.node.id + "=" + x + "x" + y);
 		});
 	}
 }
 
-function updateArguments(widgets, argumentString){
-
-	var arguments = argumentString;
-
-	for (var i = 0; i < widgets.length; i++) {
-
-		var regexp = new RegExp("pos:" + widgets[i] + "=(\\\d+)x(\\\d+)");
-
-		var widget = document.getElementById(widgets[i]);
-
-		var x = widget.style.left.replace("px", "");
-		var y = widget.style.top.replace("px", "");
-		var width = widget.offsetWidth;
-
-		// 	Ensure that the handle does not move out of the browser window.
-
-		if (y < 0) y = 0;
-		if (x < (0 - width + 20)) x = 20 - width;
-
-		if (arguments.match(regexp)) arguments = arguments.replace(regexp,"pos:" + widgets[i] + "=" + x + "x" + y);
-		else arguments = arguments + "&pos:" + widgets[i] + "=" + x + "x" + y;
-	}
-
-	return arguments;
-}
+// function updateArguments(widgets, argumentString){
+// 
+// 	var arguments = argumentString;
+// 
+// 	for (var i = 0; i < widgets.length; i++) {
+// 
+// 		var regexp = new RegExp("pos:" + widgets[i] + "=(\\\d+)x(\\\d+)");
+// 
+// 		var widget = document.getElementById(widgets[i]);
+// 
+// 		var x = widget.style.left.replace("px", "");
+// 		var y = widget.style.top.replace("px", "");
+// 		var width = widget.offsetWidth;
+// 
+// 		// 	Ensure that the handle does not move out of the browser window.
+// 
+// 		if (y < 0) y = 0;
+// 		if (x < (0 - width + 20)) x = 20 - width;
+// 
+// 		//if (arguments.match(regexp)) arguments = arguments.replace(regexp,"pos:" + widgets[i] + "=" + x + "x" + y);
+// 		//else arguments = arguments + "&pos:" + widgets[i] + "=" + x + "x" + y;
+// 		arguments = arguments + "&pos:" + widgets[i] + "=" + x + "x" + y;
+// 	}
+// 
+// 	return arguments;
+// }
